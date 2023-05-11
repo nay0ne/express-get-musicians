3cc73c1faefcef3b94c1ef0ee037d76542434ebb
Nay0ne <nayone606@googlemail.com>
2023-05-11 11:40:47 +0100

    added app.use expURL

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const { Musician } = require("../models/index")
 const { Band } = require("../models/index")
 const { db } = require("../db/connection")
 
-const port = 3000;
-
+// const port = 3000;
+// app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 //TODO: Create a GET /musicians route to return ALL musicians 
 app.get('/musicians', async (req, res) => {
     const musicians = await Musician.findAll({});
@@ -30,10 +31,17 @@ app.get('/bands/:id', async (req, res) => {
     res.json(gimme);
 });
 
+//  post request
+app.post('/musicians', async (req, res) => {
+   try { 
+        const { name, instrument } = req.body;
+        await Musician.create(req.body);
+        res.send("New musician added");
+    } catch(error) {
+        next(error);
+    }
+})
 
-// app.get('/musicians/:id', async (req, res) => {
-
-// })
 
 
 
